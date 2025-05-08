c5caeed031b4964735b81c4b8cc672c499f804fd
Lucas Henrique <lucashenriquesd@gmail.com>
2025-05-08 15:06:14 -0300

    fix: brave cursor

--- etc-nixos/configuration.nix
+++ etc-nixos/configuration.nix
@@ -37,9 +37,6 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
-
-  
-
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
@@ -93,6 +90,7 @@
   environment.variables = {
     XCURSOR_THEME = "Bibata-Modern-Classic";
     XCURSOR_SIZE = "20";
+    NIXOS_OZONE_WL = "1";
   };
 
   fonts.packages = with pkgs; [
